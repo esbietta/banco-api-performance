67b687429f11e9ff8a979d5763794814968002d8
Esbietta <esbiettad@gmail.com>
2026-03-26 09:49:56 -0300

    Criei um arquivo de configuração e apliquei variaveis de ambiente

--- test/login.test.js
+++ test/login.test.js
@@ -1,6 +1,7 @@
 import http from 'k6/http'
 import { sleep, check } from 'k6'
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
+import{pegarBaseURL} from '../utils/variaveis.js'
 
 export const options = {
 
@@ -19,7 +20,7 @@ export const options = {
 
 export default function () { 
  
-   const url = 'http://localhost:3000/login';
+   const url = pegarBaseURL()+'/login';
 
    postLogin.username = "junior.lima"
    console.log(postLogin)
